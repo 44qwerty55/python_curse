68dfe19bda1fe995728427d5b94ac2bbcf1a992a
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2024-12-29 21:34:29 +0300

    lesson 4 fix task 3

--- lesson_4/task3.py
+++ lesson_4/task3.py
@@ -7,6 +7,6 @@ inputs = ['43', '0', '-9']
 
 for input_value in inputs:
     with patch('builtins.input', return_value=input_value):
-        number = int(input("number: "))
-        actual_result = bool(number)
+        number = input("number: ")
+        actual_result = bool(int(number) > 0)
         print(f"Для входных данных '{input_value}': {actual_result}")
