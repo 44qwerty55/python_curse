21afa97287f81bbcc984ddfbc77b24068eccd13c
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2025-01-05 21:49:54 +0300

    lesson 5 add practice task 3

---
+++ lesson_5/task3.py
@@ -0,0 +1,14 @@
+
+"""
+task 3
+"""
+print("task 1:")
+input_name = 'Messi'
+input_age = 33
+print(f"Hellow {input_name.upper()}. You are {input_age} years old")
+
+print("task 2:")
+input_number1 = 1920
+input_number2 = 1080
+print(f"Screen resolution: {input_number1} X {input_number2}")
+print(f"total amount of pixels:  {input_number1 * input_number2}.")
